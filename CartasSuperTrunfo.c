d8fa40e9969b45cdc665d517108222187c22f132
andrefilho-labprogama <andrefilho.0916@gmail.com>
2026-04-07 22:02:52 +0000

    prototipo jogo!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,6 +5,16 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
+
+
+char Estado , (Uma Letra de 'A' a 'H');
+char nome da cidade[2];
+int população;
+float Área;
+float PIB;
+int Pontos Turísticos;
+
+
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
